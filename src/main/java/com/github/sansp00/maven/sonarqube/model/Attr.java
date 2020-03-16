27c18a4b0bba0fd3506c757b4a06216dd31065fe
Patrick Sansoucy <patrick.sansoucy@gmail.com>
2020-03-15 22:47:46 -0400

    Add proper HashCode/Equals/ToString, use latest pojo-tester, update codeconv token

--- src/main/java/com/github/sansp00/maven/sonarqube/model/Attr.java
+++ src/main/java/com/github/sansp00/maven/sonarqube/model/Attr.java
@@ -1,5 +1,10 @@
 package com.github.sansp00.maven.sonarqube.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -16,6 +21,21 @@ public class Attr {
 	@JsonProperty("jira-issue-key")
 	public void setJiraIssueKey(String jiraIssueKey) {
 		this.jiraIssueKey = jiraIssueKey;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return EqualsBuilder.reflectionEquals(this, other);
 	}
 
 }
--- src/main/java/com/github/sansp00/maven/sonarqube/model/Issue.java
+++ src/main/java/com/github/sansp00/maven/sonarqube/model/Issue.java
@@ -2,6 +2,11 @@ package com.github.sansp00.maven.sonarqube.model;
 
 import java.util.List;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Issue {
@@ -210,4 +215,18 @@ public class Issue {
 		this.textRange = textRange;
 	}
 
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return EqualsBuilder.reflectionEquals(this, other);
+	}
 }
